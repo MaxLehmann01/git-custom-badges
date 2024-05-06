afab60854e74280ce46b47e7337178bb9b092795
Max Lehmann <maxlehmann1404@gmail.com>
2024-05-06 22:10:24 +0200

    chore: Remove GET-VersionDynamic route and update GET-Version route to generate badge directly

--- routers/badges/index.js
+++ routers/badges/index.js
@@ -4,7 +4,6 @@ import express from "express";
 /* Routes */
 import GETHealthcheck from "./routes/GET-Healthcheck.js";
 import GETVersion from "./routes/GET-Version.js";
-import GETVersionDynamic from "./routes/GET-VersionDynamic.js";
 
 /* Constants */
 const router = express.Router();
@@ -12,7 +11,6 @@ const router = express.Router();
 /* Router-Definition */
 router.get('/healthcheck', GETHealthcheck);
 router.get('/version', GETVersion);
-router.get('/version/dynamic', GETVersionDynamic);
 
 /* Default-Export */
 export default router;
--- routers/badges/routes/GET-Version.js
+++ routers/badges/routes/GET-Version.js
@@ -1,8 +1,28 @@
+/* Utils */
+import { generateBadge } from "../../../utils/lib.js";
+
+/* Schemas */
+import Project from "../../../schemas/Project.js";
+
 /* Route-Definition */
 export default async (req, res, next) => {
   try {
-    const params = new URLSearchParams({ ...req.query, version: Date.now( )});
-    res.redirect(`${process.env.GCB_URI}/badges/version/dynamic?` + params.toString());
+    let resData = { httpStatus: 500, data: { err: undefined }};
+    const { project: projectName, custom_key } = req.query;
+
+    if(!projectName) throw new Error('invalid project provided');
+
+    const project = await Project.findOne({ name: projectName });
+    if(!project) throw new Error('invalid project provided');
+    else resData.data = generateBadge(custom_key || 'version', project.version, 'info');
+    
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    
+    resData.httpStatus = 200;
+    res.send(resData.data)
   } catch (err) {
     next(err);
   }
